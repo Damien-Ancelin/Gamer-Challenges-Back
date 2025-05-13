37986be46af955b1aa90696aa8948df472e62722
damien <ancelin.damien@gmail.com>
2025-05-13 19:29:41 +0200

    feat: update nodemon configuration, add tsconfig-paths dependency, and enhance TypeScript settings

--- src/Controllers/testController.ts
+++ src/Controllers/testController.ts
@@ -1,7 +1,7 @@
 import type { Request, Response } from 'express';
 
 export const testController = {
-  getTest: (req: Request, res: Response) => {
+  getTest: (_req: Request, res: Response) => {
     res.send('Hello, woOoOrld!');
   }
 };
--- src/Routes/index.ts
+++ src/Routes/index.ts
@@ -1,8 +1,6 @@
 import { Router } from 'express';
 import { testController } from '../Controllers/testController';
 
-import type { Request, Response } from 'express';
-
 export const router = Router();
 
 router.get('/', testController.getTest);
