035c81ea47d27a14b05e8e0c83f320bc4c6e6cdc
Damien <ancelin.damien@gmail.com>
2025-05-26 12:29:55 +0200

    feat: refactor cloudinaryService to enhance image upload methods; add transformation options for avatar and challenge images; improve error logging

--- src/services/cloudinaryService.ts
+++ src/services/cloudinaryService.ts
@@ -5,15 +5,39 @@ import debug from "debug";
 const cloudinaryDebug = debug("service:cloudinaryService");
 
 export const cloudinaryService = {
+
+  // * Upload avatar image
   async uploadAvatar(filePath: string): Promise<string> {
     try {
       const result = await cloudinary.uploader.upload(filePath, {
         folder: "avatars",
+        transformation: [
+          { width: 240, height: 240, crop: "fill" },
+          { quality: "auto", fetch_format: "auto" },
+        ],
       });
       await fs.unlink(filePath);
       return result.secure_url;
     } catch (error) {
-      cloudinaryDebug("❌ Erreur upload Cloudinary:", error);
+      cloudinaryDebug("❌ Erreur upload avatar Cloudinary:", error);
+      throw error;
+    }
+  },
+
+  // * Upload challenge image
+  async uploadChallengeImage(filePath: string): Promise<string> {
+    try {
+      const result = await cloudinary.uploader.upload(filePath, {
+        folder: "challenges_images",
+        transformation: [
+          { width: 240, height: 240, crop: "fill" },
+          { quality: "auto", fetch_format: "auto" },
+        ],
+      });
+      await fs.unlink(filePath);
+      return result.secure_url;
+    } catch (error) {
+      cloudinaryDebug("❌ Erreur upload challenge image Cloudinary:", error);
       throw error;
     }
   },
